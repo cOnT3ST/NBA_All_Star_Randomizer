1a31387b94542f43a8f04f3259ee2ebbdb73c833
a2kat <a2kat_90@mail.ru>
2020-11-19 19:24:09 +0300

    First sketch

---
+++ main.py
@@ -0,0 +1,49 @@
+import random
+
+db = [{"name": "Stephen Curry", "pos": ["point guard"], "team": "Golden State Warriors", "pic": "", "status": "not_picked"},
+	  {"name": "Kobe Bryant", "pos": ["small forward", "shooting guard"], "team": "Los Angeles Lakers", "pic": "", "status": "not_picked"},
+	  {"name": "Kevin Durant", "pos": ["power forward", "small forward", "shooting guard"], "team": "Oklahoma City Thunder", "pic": "", "status": "not_picked"},
+	  {"name": "Blake Griffin", "pos": ["power forward"], "team": "Los Angeles Clippers", "pic": "", "status": "not_picked"},
+	  {"name": "Kevin Love", "pos": ["center", "power forward"], "team": "Minnesota Timberwolves", "pic": "", "status": "not_picked"},
+	  {"name": "Kyrie Irving", "pos": ["point guard"], "team": "Cliveland Cavaliers", "pic": "", "status": "not_picked"},
+	  {"name": "Dwyane Wade", "pos": ["point guard", "shooting guard"], "team": "Miami Heat", "pic": "", "status": "not_picked"},
+	  {"name": "Paul George", "pos": ["small forward", "shooting guard"], "team": "Indiana Pacers", "pic": "", "status": "not_picked"},
+	  {"name": "LeBron James", "pos": ["power forward", "small forward", "point guard", "shooting guard"], "team": "Miami Heat", "pic": "", "status": "not_picked"},
+	  {"name": "Carmelo Anthony", "pos": ["power forward", "small forward"], "team": "New York Knicks", "pic": "", "status": "not_picked"},
+	  {"name": "Chris Paul", "pos": ["point guard"], "team": "Los Angeles Clippers", "pic": "", "status": "not_picked"},
+	  {"name": "Dwight Howard", "pos": ["center", "power forward"], "team": "Houston Rockets", "pic": "", "status": "not_picked"},
+	  {"name": "Dirk Nowitzki", "pos": ["center", "power forward"], "team": "Dallas Mavericks", "pic": "", "status": "not_picked"},
+	  {"name": "LaMarcus Aldridge", "pos": ["center", "power forward"], "team": "Portland Trail Blazers", "pic": "", "status": "not_picked"},
+	  {"name": "Damian Lillard", "pos": ["point guard"], "team": "Portland Trail Blazers", "pic": "", "status": "not_picked"},
+	  {"name": "James Harden", "pos": ["point guard", "shooting guard"], "team": "Houston Rockets", "pic": "", "status": "not_picked"},
+	  {"name": "John Wall", "pos": ["point guard"], "team": "Washington Wizards", "pic": "", "status": "not_picked"},
+	  {"name": "DeMar DeRozan", "pos": ["small forward", "shooting guard"], "team": "Toronto Raptors", "pic": "", "status": "not_picked"},
+	  {"name": "Paul Millsap", "pos": ["power forward"], "team": "Atlanta Hawks", "pic": "", "status": "not_picked"},
+	  {"name": "Joakim Noah", "pos": ["center"], "team": "Chicago Bulls", "pic": "", "status": "not_picked"},
+	  {"name": "Roy Hibbert", "pos": ["center"], "team": "Indiana Pacers", "pic": "", "status": "not_picked"},
+	  {"name": "Chris Bosh", "pos": ["center", "power forward"], "team": "Miami Heat", "pic": "", "status": "not_picked"},
+	  {"name": "Joe Johnson", "pos": ["small forward", "shooting guard"], "team": "Brooklyn Nets", "pic": "", "status": "not_picked"}]
+
+class Player:
+	def __init__(self, pl_dict):
+		self.name = pl_dict['name']
+		self.pos = pl_dict['pos']
+		self.team = pl_dict['team']
+		self.pic = pl_dict['pic']
+
+	def __str__(self):
+		return f'{self.name} from {self.team} to be playing as a {random.choice(self.pos)}'
+
+class Team:
+	formation = ("point guard", "shooting guard", "small forward", "power forward", "center")
+
+	def __init__(self, name, roster = []):
+		self.name = name
+		self.roster = roster
+
+	def __str__(self):
+		str_team = f'{self.name} \n'
+		for player in self.roster:
+			str_team += f'{player.pos}: {player.name} \n'
+		str_team = str_team[:-1]
+		return str_team
